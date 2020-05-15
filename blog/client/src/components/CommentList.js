35e183022e7debc5584938a567a06b478a7db9f5
Danilo Canivel <d.canivel@gmail.com>
2020-05-15 15:30:28 -0400

    simple services

---
+++ blog/client/src/components/CommentList.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default ({ postId }) => {
+  const [comments, setComments] = useState([]);
+
+  const fetchData = async () => {
+    const res = await axios.get(
+      `http://localhost:4001/posts/${postId}/comments`
+    );
+    setComments(res.data);
+    //console.log(res);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const renderedComments = comments.map((comment) => {
+    console.log(comment.id);
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
